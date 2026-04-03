cd3edba029ffb865041e8e961729ffb4c928b252
Matthew Cosensci <matt@hivewerks.com>
2026-04-02 19:44:54 -0700

    Initial Commit

---
+++ The Damn Weather/The_Damn_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Damn_WeatherApp.swift
+//  The Damn Weather
+//
+//  Created by Matthew Cosensci on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct The_Damn_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
